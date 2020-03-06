82abc8d7b364bebff2c9818624e98364924afa87
Tanvirul islam <55061568+Tanvirulislam345@users.noreply.github.com>
2020-03-06 15:47:39 +0600

    Add files via upload

---
+++ part2/Using_Variables.java
@@ -0,0 +1,33 @@
+package part2;
+
+public class Using_Variables {	
+//Different type of variables	
+	public static void main(String[] args) {
+		
+		//Declare and assign different type of variables
+		 int myNumber = 12;
+		  short myShort = 456;
+		  long myLong = 34556;
+		  
+		  float myFloat = 2345.7f;
+		  double myDouble = 4.5678;
+		  
+		  char myChar = 'y';
+		  boolean myBoolean = true;
+		  
+		  byte myByte = 127;
+		  
+		//Displaying result different type of variables
+		  System.out.println(myNumber);
+		  System.out.println(myShort);
+		  System.out.println(myLong);
+		  System.out.println(myFloat);
+		  System.out.println(myDouble);
+		  System.out.println(myChar);
+		  System.out.println(myBoolean);
+		  System.out.println(myByte);
+
+
+	}
+
+}
