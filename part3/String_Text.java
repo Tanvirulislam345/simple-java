7964218dcb667ccba07c21929d069ec5b84db2a0
Tanvirul islam <55061568+Tanvirulislam345@users.noreply.github.com>
2020-03-06 15:48:25 +0600

    Add files via upload

---
+++ part3/String_Text.java
@@ -0,0 +1,32 @@
+package part3;
+
+public class String_Text {
+//String type variables.
+	public static void main(String[] args) {
+
+		//Declare & assign some variables
+		int number = 7;
+		float Number = 789.8f;
+		double add;
+		
+		add = number+Number;
+		
+		//Declare String type variables and assign some names.
+		String text = "tanvir";
+		String blank = " ";
+		String name = "bob";
+		
+		String greeting = text+blank+name;
+		
+		//Displaying result
+		System.out.println(text);
+		System.out.println(greeting);
+		
+		System.out.println(add);
+		
+		System.out.println("My integer number is : "+number);
+
+
+	}
+
+}
