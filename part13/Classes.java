1fedf480bd38017951abacb03622fb428d40990f
Tanvirul islam <55061568+Tanvirulislam345@users.noreply.github.com>
2020-03-06 22:32:47 +0600

    Add files via upload

---
+++ part13/Classes.java
@@ -0,0 +1,54 @@
+package part13;
+
+	//create a class and using some data
+	class Person{
+		/*
+		*class can contain 2 thing.
+		*
+		 1. field (data item)
+		 2. methods/subroutines(function)
+		 
+		*****/
+		
+		
+		//declaring Instance variable
+		
+		String name;
+		int age;
+		
+	}
+
+
+public class Classes {
+	
+		public static void main(String[] args) {
+			
+			
+			//create  Person1,2,3 object using person class
+			Person person1 = new Person();
+			Person person2 = new Person();
+			Person person3 = new Person();
+			
+			
+			//assign some value in person data type(person is object) .
+			person1.name = "Tanvirul Islam";
+			person1.age = 22;
+			
+			person2.name = "Hasibul Islam";
+			person2.age = 22;
+			
+			person3.name = "Soniya Islam";
+			person3.age = 22;
+			
+			
+			System.out.println(person1.name);
+			System.out.println(person1.age);
+			System.out.println(person2.name);
+			System.out.println(person2.age);
+			System.out.println(person3.name);
+			System.out.println(person3.age);
+		
+
+		}
+
+	}
