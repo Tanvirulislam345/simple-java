df69fa8bda64b562310ddfe6c86c2a44f0274263
Tanvirul islam <55061568+Tanvirulislam345@users.noreply.github.com>
2020-03-06 15:47:19 +0600

    Add files via upload

---
+++ part1/HelloWorld.java
@@ -0,0 +1,12 @@
+package part1;
+
+public class HelloWorld {
+//Hello world program.
+	public static void main(String[] args) {
+		
+				//Displaying result.
+				System.out.println("Hello world");
+		
+	}
+
+}
