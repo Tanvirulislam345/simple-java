16244b27a04b34832737cab88b1ac21c7fed661f
Tanvirul islam <55061568+Tanvirulislam345@users.noreply.github.com>
2020-03-06 15:49:25 +0600

    Add files via upload

---
+++ part4/Whhile_loop.java
@@ -0,0 +1,25 @@
+package part4;
+
+public class Whhile_loop {
+//While loop program.
+	public static void main(String[] args) {
+		
+		//Declare and initialing value
+		int value = 0;
+		
+		//using While loop
+		while(value < 10)
+		{
+			
+		System.out.println("hello" + value);
+		value = value + 1;
+		
+		/*using condition for break */
+		if(value == 5)
+		break;
+		
+		}
+
+	}
+
+}
