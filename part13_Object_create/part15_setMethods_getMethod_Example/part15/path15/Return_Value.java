a10fc23e8d436a5edac65ddc678fe6f02bc2addb
Tanvirul islam <55061568+Tanvirulislam345@users.noreply.github.com>
2020-03-09 08:43:51 +0600

    Add files via upload

---
+++ part13_Object_create/part15_setMethods_getMethod_Example/part15/path15/Return_Value.java
@@ -0,0 +1,12 @@
+package path15;
+
+//create a class
+public class Return_Value {
+	
+	//define methods
+	int square(int value) {
+		
+		return value*value;
+	}
+
+}
---
+++ part13_Object_create/part15_setMethods_getMethod_Example/part15/path15/Test_return_Value.java
@@ -0,0 +1,18 @@
+package path15;
+
+public class Test_return_Value {
+
+	public static void main(String[] args) {
+
+		//create an object
+		Return_Value sam = new Return_Value();
+	
+		
+		//passing value
+		//int result = sam.square(5);
+		//System.out.println("Square result : "+ result);
+		System.out.println("Square result : "+ sam.square(5));
+
+	}
+
+}
