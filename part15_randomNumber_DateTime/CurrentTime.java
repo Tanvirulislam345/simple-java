14bac3358e8de93e38d59d19edb752c56da351d9
Tanvirul islam <55061568+Tanvirulislam345@users.noreply.github.com>
2021-05-07 12:54:44 +0600

    Update CurrentTime.java

--- part15_randomNumber_DateTime/CurrentTime.java
+++ part15_randomNumber_DateTime/CurrentTime.java
@@ -12,22 +12,23 @@ public class CurrentTime {
 
 	//this is simple way find date and time.
         //using LoclDateTime and now methods find time or date
+	    
+//         LocalDateTime date = LocalDateTime.now();
         LocalDateTime time = LocalDateTime.now();
-        LocalDateTime date = LocalDateTime.now();
 
 	//use format
-        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+//         DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy/MM/dd");
         DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yy:mm:ss");
 	
 	//current date and time
-        String currentDate = formatter1.format(date);
+//         String currentDate = formatter1.format(date);
         String currentTime = formatter2.format(time);
 
 	//print
-        System.out.println("Current Date is - " + currentDate);
+//         System.out.println("Current Date is - " + currentDate);
         System.out.println("Current time is - " + currentTime);
 
-        
+//         This is another way find currenttime
         //using LoclTime and now methods
         LocalTime time = LocalTime.now();
         //System.out.println("Current time is - "+time);
